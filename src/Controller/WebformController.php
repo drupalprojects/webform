983c82c48e506a7ac03d77fcccf3533e1b944b5b
Saul Willers <saul@saulwillers.com>
2015-03-10 20:00:17 -0300

    Issue #2416241 by DanChadwick: Use views to display webform list in content menu.

--- src/Controller/WebformController.php
+++ src/Controller/WebformController.php
@@ -18,6 +18,12 @@ class WebformController extends ControllerBase {
    * Create an overview of webform content.
    */
   public function contentOverview() {
+    // Determine whether views or hard-coded tables should be used for the webforms table.
+    if (!webform_variable_get('webform_table')) {
+      $view = views_get_view('webform_webforms');
+      return $view->preview('default');
+    }
+
     $query = db_select('webform', 'w');
     $query->join('node', 'n', 'w.nid = n.nid');
     $query->fields('n');
