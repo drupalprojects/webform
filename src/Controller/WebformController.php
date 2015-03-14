754452a7b5d9bac45bb8336bc6600ca61fc1b1ef
Saul Willers <saul@saulwillers.com>
2015-03-14 10:42:35 -0300

    Issue #2447655 by fenstrat: Update WebformController::contentOverview to use _controller and routes

--- src/Controller/WebformController.php
+++ src/Controller/WebformController.php
@@ -8,6 +8,8 @@
 namespace Drupal\webform\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+use Drupal\node\Entity\Node;
 
 /**
  * Returns responses for Webform routes.
@@ -18,18 +20,58 @@ class WebformController extends ControllerBase {
    * Create an overview of webform content.
    */
   public function contentOverview() {
-    // Determine whether views or hard-coded tables should be used for the webforms table.
-    if (!webform_variable_get('webform_table')) {
-      $view = views_get_view('webform_webforms');
-      return $view->preview('default');
-    }
-
+    // @todo This needs to be removed and the view webform_webforms used instead.
     $query = db_select('webform', 'w');
     $query->join('node', 'n', 'w.nid = n.nid');
     $query->fields('n');
     $nodes = $query->execute()->fetchAllAssoc('nid');
     module_load_include('inc', 'webform', 'includes/webform.admin');
-    return _theme('webform_admin_content', array('nodes' => $nodes));
+
+    $header = array(
+      t('Title'),
+      array('data' => t('View'), 'colspan' => '4'),
+      array('data' => t('Operations'), 'colspan' => '3')
+    );
+
+    $rows = array();
+    if (!empty($nodes)) {
+      foreach ($nodes as $node) {
+        $node = Node::load($node->nid);
+        $rows[] = array(
+          \Drupal::l($node->getTitle(), Url::fromRoute('entity.node.canonical', ['node' => $node->id()])),
+          t('Submissions'), //l(t('Submissions'), 'node/' . $node->nid . '/webform-results'),
+          t('Analysis'), //l(t('Analysis'), 'node/' . $node->nid . '/webform-results/analysis'),
+          t('Table'), //l(t('Table'), 'node/' . $node->nid . '/webform-results/table'),
+          t('Download'), //l(t('Download'), 'node/' . $node->nid . '/webform-results/download'),
+          $node->access('update') ? \Drupal::l(t('Edit'), Url::fromRoute('entity.node.edit_form', ['node' => $node->id()])) : '',
+          t('Components'), //node_access('update', $node) ? l(t('Components'), 'node/' . $node->nid . '/webform') : '',
+          t('Clear'), //user_access('delete all webform submissions') ? l(t('Clear'), 'node/' . $node->nid . '/webform-results/clear') : '',
+        );
+      }
+    }
+
+    if (empty($nodes)) {
+      $webform_types = webform_node_types();
+      if (empty($webform_types)) {
+        $message = t('Webform is currently not enabled on any content types.') . ' ' . t('Visit the <a href="!url">Webform settings</a> page and enable Webform on at least one content type.', array('!url' => Url::fromRoute('webform.settings')));
+      }
+      else {
+        $webform_type_list = webform_admin_type_list();
+        $message = t('There are currently no webforms on your site. Create a !types piece of content.', array('!types' => $webform_type_list));
+      }
+
+      $rows[] = array(
+        array('data' => $message, 'colspan' => 7),
+      );
+    }
+
+    $table = array(
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+    );
+
+    return $table;
   }
 
 }
