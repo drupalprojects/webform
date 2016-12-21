ee65d3c927fa5b93f0159ab77b596893bf9e07d1
jrockowitz <jrockowitz@gmail.com>
2016-12-21 05:25:37 -0500

    Fix broken test

--- src/Tests/WebformEntityElementsValidationUnitTest.php
+++ src/Tests/WebformEntityElementsValidationUnitTest.php
@@ -18,7 +18,7 @@ class WebformEntityElementsValidationUnitTest extends KernelTestBase {
    *
    * @var array
    */
-  protected static $modules = ['system', 'webform', 'user'];
+  public static $modules = ['system', 'webform', 'user'];
 
   /**
    * The webform elements validator.
