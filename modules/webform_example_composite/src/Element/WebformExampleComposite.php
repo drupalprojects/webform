8f1114e3cb1cfe4605c01e5e46838644b3037aa9
jrockowitz <jrockowitz@371407.no-reply.drupal.org>
2017-07-20 09:36:25 -0400

    Issue #2896091 by jrockowitz: Custom composite element with states

--- modules/webform_example_composite/src/Element/WebformExampleComposite.php
+++ modules/webform_example_composite/src/Element/WebformExampleComposite.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\webform_example_composite\Element;
 
+use Drupal\Component\Utility\Html;
 use Drupal\webform\Element\WebformCompositeBase;
 
 /**
@@ -32,24 +33,41 @@ class WebformExampleComposite extends WebformCompositeBase {
    * {@inheritdoc}
    */
   public static function getCompositeElements() {
+    // Generate an unique ID that can be used by #states.
+    $html_id = Html::getUniqueId('webform_example_composite');
+
     $elements = [];
     $elements['first_name'] = [
       '#type' => 'textfield',
       '#title' => t('First name'),
+      '#attributes' => ['data-webform-composite-id' => $html_id . '--first_name'],
     ];
     $elements['last_name'] = [
       '#type' => 'textfield',
       '#title' => t('Last name'),
+      '#attributes' => ['data-webform-composite-id' => $html_id . '--last_name'],
     ];
     $elements['date_of_birth'] = [
       '#type' => 'date',
       '#title' => t('Date of birth'),
+      '#states' => [
+        'enabled' => [
+          '[data-webform-composite-id="' . $html_id . '--first_name"]' => ['filled' => TRUE],
+          '[data-webform-composite-id="' . $html_id . '--last_name"]' => ['filled' => TRUE],
+        ],
+      ],
     ];
     $elements['gender'] = [
       '#type' => 'select',
       '#title' => t('Gender'),
       '#options' => 'gender',
       '#empty_option' => '',
+      '#states' => [
+        'enabled' => [
+          '[data-webform-composite-id="' . $html_id . '--first_name"]' => ['filled' => TRUE],
+          '[data-webform-composite-id="' . $html_id . '--last_name"]' => ['filled' => TRUE],
+        ],
+      ],
     ];
     return $elements;
   }
