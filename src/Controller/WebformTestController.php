7180fedfeb6ec656c7a35f31eebb32aa42475a11
jrockowitz <jrockowitz@371407.no-reply.drupal.org>
2017-08-12 06:59:23 -0400

    Issue #2901712 by jrockowitz: Remove WebformTestController and merge code into WebformSubmissionForm

--- src/Controller/WebformTestController.php
+++ src/Controller/WebformTestController.php
@@ -73,11 +73,6 @@ class WebformTestController extends ControllerBase implements ContainerInjection
       $values['entity_id'] = $source_entity->id();
     }
 
-    // @todo Determine why we are checking for the webform id as querystring parameter.
-    if ($request->query->get('webform_id') == $webform->id()) {
-      return $webform->getSubmissionForm($values);
-    }
-
     // Generate date.
     $values['data'] = $this->generate->getData($webform);
     return $webform->getSubmissionForm($values, 'test');
