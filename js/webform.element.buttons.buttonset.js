f22e3dfff08071e9db127da7b9f5ffadfb89715a
jrockowitz <jrockowitz@371407.no-reply.drupal.org>
2017-09-25 21:28:19 -0400

    Issue #2911660 by jrockowitz: Buttons element uses deprecated buttonset which is not support in Drupal 8.4.x+

--- js/webform.element.buttons.buttonset.js
+++ js/webform.element.buttons.buttonset.js
@@ -1,6 +1,6 @@
 /**
  * @file
- * JavaScript behaviors for jQuery UI buttons element integration.
+ * JavaScript behaviors for jQuery UI buttons (buttonset) element integration.
  */
 
 (function ($, Drupal) {
@@ -8,18 +8,21 @@
   'use strict';
 
   /**
-   * Create jQuery UI buttons element.
+   * Create jQuery UI buttons (buttonset) element.
    *
    * @type {Drupal~behavior}
    */
-  Drupal.behaviors.webformButtons = {
+  Drupal.behaviors.webformButtonsButtonSet = {
     attach: function (context) {
       $(context).find('.js-webform-buttons .form-radios, .js-webform-buttons.form-radios, .js-webform-buttons .js-webform-radios').once('webform-buttons').each(function () {
         var $input = $(this);
+
         // Remove all div and classes around radios and labels.
         $input.html($input.find('input[type="radio"], label').removeClass());
+
         // Create buttonset.
         $input.buttonset();
+
         // Disable buttonset.
         $input.buttonset('option', 'disabled', $input.find('input[type="radio"]:disabled').length);
 
