ef4b28c37a1e8760e4ae579a3b1955acb1830d27
keeganstreet <keeganstreet@3528247.no-reply.drupal.org>
2017-10-17 10:14:13 -0400

    Issue #2890812 by andrewmacpherson, keeganstreet: webformDisableAutoSubmit behaviour should not affect buttons

--- js/webform.element.signature.js
+++ js/webform.element.signature.js
@@ -28,7 +28,7 @@
         var value = $input.val();
         var $wrapper = $input.parent();
         var $canvas = $wrapper.find('canvas');
-        var $button = $wrapper.find(':button, input[type="submit"]');
+        var $button = $wrapper.find(':button, :submit');
         var canvas = $canvas[0];
         // Set height.
         $canvas.attr('width', $wrapper.width());
