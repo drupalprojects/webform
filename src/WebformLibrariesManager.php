0031fe1921b7d7c7acbf136dcea2539fd769752d
jrockowitz <jrockowitz@371407.no-reply.drupal.org>
2017-11-02 15:10:35 -0400

    Issue #2920513 by jrockowitz: CKEDITOR: autogrow and undefinedlink plugin not found

--- src/WebformLibrariesManager.php
+++ src/WebformLibrariesManager.php
@@ -249,6 +249,15 @@ class WebformLibrariesManager implements WebformLibrariesManagerInterface {
     $ckeditor_version = $core_libraries['ckeditor']['version'];
 
     $libraries = [];
+    $libraries['ckeditor.autogrow'] = [
+      'title' => $this->t('CKEditor: Autogrow'),
+      'description' => $this->t('Automatically expand and shrink vertically depending on the amount and size of content entered in its editing area.'),
+      'notes' => $this->t('Allows CKEditor to automatically expand and shrink vertically '),
+      'homepage_url' => Url::fromUri('https://ckeditor.com/addon/autogrow'),
+      'download_url' => Url::fromUri("https://download.ckeditor.com/autogrow/releases/autogrow_$ckeditor_version.zip"),
+      'version' => $ckeditor_version,
+      'optional' => TRUE,
+    ];
     $libraries['ckeditor.fakeobjects'] = [
       'title' => $this->t('CKEditor: Fakeobjects'),
       'description' => $this->t('Utility required by CKEditor link plugin.'),
