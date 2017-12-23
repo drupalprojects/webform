d672c3f931c8d1e5b3866ba46e6ccf2491b72522
jrockowitz <jrockowitz@gmail.com>
2017-12-23 16:29:05 -0500

    Update 'Add element' page title to include the element's title.

--- modules/webform_ui/src/Form/WebformUiElementAddForm.php
+++ modules/webform_ui/src/Form/WebformUiElementAddForm.php
@@ -16,6 +16,8 @@ class WebformUiElementAddForm extends WebformUiElementFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state, WebformInterface $webform = NULL, $key = NULL, $parent_key = NULL, $type = NULL) {
     $this->webform = $webform;
+    $this->element['#type'] = $type;
+    $this->action = $this->t('created');
 
     $parent_key = $this->getRequest()->get('parent');
     if ($parent_key) {
@@ -25,8 +27,10 @@ class WebformUiElementAddForm extends WebformUiElementFormBase {
       }
     }
 
-    $this->element['#type'] = $type;
-    $this->action = $this->t('created');
+    $element_plugin = $this->getWebformElementPlugin();
+
+    $form['#title'] = $this->t('Add @label element', ['@label' => $element_plugin->getPluginLabel()]);
+
     $form = parent::buildForm($form, $form_state, $webform, NULL, $parent_key);
     if (isset($form['properties']['element']['title'])) {
       $form['properties']['element']['title']['#attributes']['autofocus'] = 'autofocus';
