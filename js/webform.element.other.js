377f22fa68d99d2ed9b88e122417f961232a73d1
jrockowitz <jrockowitz@371407.no-reply.drupal.org>
2018-02-14 19:52:51 -0500

    Issue #2944716 by jrockowitz, Ahmad Abbad: Element type ' buttons other ' is not supported with bootstrap theme

--- js/webform.element.other.js
+++ js/webform.element.other.js
@@ -139,7 +139,7 @@
 
         var $buttons = $element.find('input[type="radio"]');
         var $input = $element.find('.js-webform-buttons-other-input');
-        var $container = $(this).find('.form-radios');
+        var $container = $(this).find('.js-webform-webform-buttons');
 
         // Create set onchange handler.
         $container.change(function () {
