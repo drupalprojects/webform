5f5c7bda0c9db3cf389568f6d2533fefa91d188c
andypost <andypost@118908.no-reply.drupal.org>
2018-02-28 12:03:32 -0500

    Issue #2945890 by jrockowitz, piggito, andypost: Html required validation not working for radios element

--- src/Plugin/WebformElement/Radios.php
+++ src/Plugin/WebformElement/Radios.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\webform\Plugin\WebformElement;
 
+use Drupal\webform\WebformSubmissionInterface;
+
 /**
  * Provides a 'radios' element.
  *
@@ -26,6 +28,18 @@ class Radios extends OptionsBase {
       // iCheck settings.
       'icheck' => '',
     ] + parent::getDefaultProperties();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepare(array &$element, WebformSubmissionInterface $webform_submission = NULL) {
+    parent::prepare($element, $webform_submission);
+
+    // Issue #2856795: If radio buttons are required but not filled form is
+    // nevertheless submitted.
+    // Issue #2856315: Conditional Logic - Requiring Radios in a Fieldset.
+    $element['#attached']['library'][] = 'webform/webform.element.radios';
   }
 
 }
