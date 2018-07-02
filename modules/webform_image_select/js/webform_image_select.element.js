36925ae803307760e0ebeee6d82f9782ed285cac
wesleydv <wesleydv@118660.no-reply.drupal.org>
2018-07-02 08:46:55 -0400

    Issue #2958102 by wesleydv: webform update path needs to run after system_update_8501 when updating to 8.5.x at the same time

--- modules/webform_image_select/js/webform_image_select.element.js
+++ modules/webform_image_select/js/webform_image_select.element.js
@@ -25,6 +25,7 @@
 
       $('.js-webform-image-select', context).once('webform-image-select').each(function () {
         var $select = $(this);
+        var isMultiple = $select.attr('multiple');
 
         // Apply image data to options.
         var images = JSON.parse($select.attr('data-images'));
@@ -50,6 +51,48 @@
         }
 
         $select.imagepicker(options);
+
+        // Add very basic accessibility to the image picker by
+        // enabling tabbing and toggling via the spacebar.
+        // @see https://github.com/rvera/image-picker/issues/108
+
+        if (isMultiple) {
+          $select.next('.image_picker_selector').attr('role', 'radiogroup');
+        }
+
+        $select.next('.image_picker_selector').find('.thumbnail')
+          .prop('tabindex', '0')
+          .attr('role', isMultiple ? 'checkbox' : 'radio')
+          .each(function() {
+            $(this).attr('title', $(this).find('img').attr('alt'));
+          })
+          .on('focus', function(event) {
+            $(this).addClass('focused');
+          })
+          .on('blur', function(event) {
+            $(this).removeClass('focused');
+          })
+          .on('keydown', function(event) {
+            if(event.which === 32) {
+              // Space.
+              $(this).click();
+              event.preventDefault();
+            }
+            else if(event.which === 37 || event.which === 38) {
+              // Left or Up.
+              $(this).parent().prev().find('.thumbnail').focus();
+              event.preventDefault();
+            }
+            else if(event.which === 39 || event.which === 40) {
+              // Right or Down.
+              $(this).parent().next().find('.thumbnail').focus();
+              event.preventDefault();
+            }
+          })
+          .on('click', function(event) {
+            var selected = $(this).hasClass('selected');
+            $(this).attr('aria-checked', selected);
+          });
       });
     }
   };
