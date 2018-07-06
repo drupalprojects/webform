0b854e3883515cf6ca584cb6212e47aeb88cfc79
jrockowitz <jrockowitz@371407.no-reply.drupal.org>
2018-07-06 07:02:01 -0400

    Issue #2984147 by jrockowitz: [accessibility] CodeMirror does not allow users to exit editor via keyboard

--- js/webform.element.codemirror.js
+++ js/webform.element.codemirror.js
@@ -40,12 +40,27 @@
           lineNumbers: true,
           viewportMargin: Infinity,
           readOnly: ($(this).prop('readonly') || $(this).prop('disabled')) ? true : false,
-          // Setting for using spaces instead of tabs - https://github.com/codemirror/CodeMirror/issues/988
           extraKeys: {
+            // Setting for using spaces instead of tabs - https://github.com/codemirror/CodeMirror/issues/988
             Tab: function (cm) {
               var spaces = Array(cm.getOption('indentUnit') + 1).join(' ');
               cm.replaceSelection(spaces, 'end', '+element');
+            },
+            // On 'Escape' move to the next tabbable input.
+            // @see http://bgrins.github.io/codemirror-accessible/
+            Esc: function (cm) {
+              // Must show and then textarea so that we can determine
+              // its tabindex.
+              var textarea = $(cm.getTextArea());
+              $(textarea).show().addClass('visually-hidden');
+              var $tabbable = $(':tabbable');
+              var tabindex = $tabbable.index(textarea);
+              $(textarea).hide().removeClass('visually-hidden');
+
+              // Tabindex + 2 accounts for the CodeMirror's iframe.
+              $tabbable.eq(tabindex + 2).focus();
             }
+
           }
         }, Drupal.webform.codeMirror.options);
 
