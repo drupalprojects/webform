2d433f0a2b913c1c502864782faf2674cb4ab319
jrockowitz <jrockowitz@gmail.com>
2018-07-14 18:56:03 -0400

    Remove double quotes from JS

--- js/webform.jquery.ui.dialog.js
+++ js/webform.jquery.ui.dialog.js
@@ -24,15 +24,15 @@
    * Never focus dialog on first link.
    */
   $.ui.dialog.prototype._focusTabbable = function() {
-    var hasFocus = this.element.find("[autofocus]");
+    var hasFocus = this.element.find('[autofocus]');
     if (!hasFocus.length) {
-      hasFocus = this.element.find(":tabbable:not(a.webform-element-help)");
+      hasFocus = this.element.find(':tabbable:not(a.webform-element-help)');
     }
     if (!hasFocus.length) {
-      hasFocus = this.uiDialogButtonPane.find(":tabbable:not(a.webform-element-help)");
+      hasFocus = this.uiDialogButtonPane.find(':tabbable:not(a.webform-element-help)');
     }
     if (!hasFocus.length) {
-      hasFocus = this.uiDialogTitlebarClose.filter(":tabbable:not(a.webform-element-help)");
+      hasFocus = this.uiDialogTitlebarClose.filter(':tabbable:not(a.webform-element-help)');
     }
     if (!hasFocus.length) {
       hasFocus = this.uiDialog;
