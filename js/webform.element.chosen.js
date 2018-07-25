790df251d84b1f3c7ef8a202d5af28e43cf3f3b8
jrockowitz <jrockowitz@371407.no-reply.drupal.org>
2018-07-25 11:51:09 -0400

    Issue #2987684 by jrockowitz, oheller: Client-side-validation broken with Chosen

--- js/webform.element.chosen.js
+++ js/webform.element.chosen.js
@@ -23,12 +23,40 @@
         return;
       }
 
+      // Add HTML5 required attribute support.
+      // Checking for $oldChosen to prevent duplicate workarounds from
+      // being applied.
+      // @see https://github.com/harvesthq/chosen/issues/515
+      if (!$.fn.oldChosen) {
+        $.fn.oldChosen = $.fn.chosen;
+        $.fn.chosen = function(options) {
+          var select = $(this), is_creating_chosen = !!options;
+          if (is_creating_chosen && select.css('position') === 'absolute') {
+            select.removeAttr('style');
+          }
+          var ret = select.oldChosen(options);
+          if (is_creating_chosen && select.css('display') === 'none') {
+            select.attr('style','display:visible; position:absolute; width:0px; height: 0px; clip:rect(0,0,0,0)');
+            select.attr('tabindex', -1);
+          }
+          return ret;
+        };
+      }
+
       var options = $.extend({width: '100%'}, Drupal.webform.chosen.options);
 
       $(context)
         .find('select.js-webform-chosen, .js-webform-chosen select')
         .once('webform-chosen')
-        .chosen(options);
+        .each(function () {
+          var $select = $(this);
+          // Check for .chosen-enable to prevent the chosen.module and
+          // webform.module from both initializing the chosen select element.
+          if (!$select.hasClass('chosen-enable')) {
+            $select.chosen(options);
+          }
+        })
+
     }
   };
 
